5e2424532aec4fbd30839d2ceaebd8123a8cccfd
Admin <tinh.pham@ipicorp.co>
2020-10-20 23:05:04 +0700

    ok man 1

--- src/main/java/com/example/demo/rest/TestBranch.java
+++ src/main/java/com/example/demo/rest/TestBranch.java
@@ -3,7 +3,7 @@ package com.example.demo.rest;
 public class TestBranch {
     //do something
     public void doSomeThing(){
-        System.out.println("ok mman");
+        System.out.println("ok mmanm, that's right");
     }
 
 }
